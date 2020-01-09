5bb23e1befbfbc8967704a9f7fb82e827669afbc
Sergiom9824 <47410117+Sergiom9824@users.noreply.github.com>
2020-01-09 18:55:35 -0500

    Update joystick_meta.ino

--- joystick_meta.ino
+++ joystick_meta.ino
@@ -1,13 +1,14 @@
 // Analog Pins connected to the joystick outputs
 int Eje_X = 1;
 int Eje_Y = 2;
-int P = 3;
-int Pulsador = 0;
+int Pr = 4;
+int Pl = 5;
 void setup() {
  
   //Initialize serial communication at 9600 bps
   Serial.begin(9600);
-  pinMode(P, INPUT_PULLUP);
+  pinMode(Pr, INPUT);
+  pinMode(Pl, INPUT);
  
   }
  
@@ -18,10 +19,12 @@ void loop() {
   int DY = analogRead(Eje_Y);
 
   //Lee valor del pulsador
-  Pulsador = digitalRead(P);
- 
+  int Pulsador_rigth = digitalRead(Pr);
+  int Pulsador_left = digitalRead(Pl);
+  
+  
   //Concatenate the variables in a String
-  String Lectura = String(DX) + "/" + String(DY) + "/" + String(Pulsador);
+  String Lectura = String(DX) + "/" + String(DY) + "/" + String(Pulsador_left) + "/" + String(Pulsador_rigth);
 
   //Prints the String to the serial port
   Serial.println(Lectura);
